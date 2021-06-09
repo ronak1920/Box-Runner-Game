cf4b67cb19859c2f84cd370defa62f9eccf37837
ronak1920 <60731241+ronak1920@users.noreply.github.com>
2021-06-09 14:50:06 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC8kcNy_YDxsg9gDc0UdGW_ujZfH1EmRDg",
+  authDomain: "my-project-c7ea3.firebaseapp.com",
+  projectId: "my-project-c7ea3",
+  storageBucket: "my-project-c7ea3.appspot.com",
+  messagingSenderId: "506901208247",
+  appId: "1:506901208247:web:e124e594de12f2e0214132"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// console.log(firebase);
+
+// var database = firebase.database();
+// var ref = database.ref('highScore');
